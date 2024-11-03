0e97c3f9c0fe2ab2281d9fba4d749510bc88580d
WHY-student <3176417102@qq.com>
2024-11-03 16:35:46 +0800

    mask visualizer

--- whylab/visual/__init__.py
+++ whylab/visual/__init__.py
@@ -1,9 +1,11 @@
 from .bbox import visual_box, visual_box_from_xml
 from .tsne import visual_tsne
+from .mask import show_mask, show_mask_image
 
 __all__ = [
     'visual_box', 'visual_box_from_xml',
-    'visual_tsne'
+    'visual_tsne',
+    'show_mask', 'show_mask_image',
 ]
 
 
